27f85d9ea073092d43d49a8d58f271a0a7d9624c
Kavitha V U <kavithavu98@gmail.com>
2024-07-03 09:34:49 +0530

    Fix react-icons resolution issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-icons': path.resolve(__dirname, 'node_modules/react-icons')
+    }
+  }
 })
